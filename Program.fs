2ac57addd2e642913f84677512d309a0e88259d5
ben lim <lim6112j@gmail.com>
2023-03-11 22:45:11 +0900

    many types for domain

--- Program.fs
+++ Program.fs
@@ -1,6 +1,26 @@
 ﻿namespace DomainTypes// For more information see https://aka.ms/fsharp-console-apps
 open BusinessLogic
 module Program =
-  let point = {x=1.1; y=2.2}
-  let vehicle = {CurrentLoc=point; Id= "1"; Name=Some "lim"}
-  printfn "vehicle object : %A" vehicle
+  printfn "hello world"
+  let customerId = CustomerId 42
+  let orderId = OrderId 42
+  let (CustomerId innervalue) = customerId
+  printfn "customer id value : %i" innervalue
+  let processCustomerId (CustomerId innerV) =
+    printfn "innervalue is %i" innerV
+  processCustomerId(customerId)
+  //printfn "customer id = order id : %b" (customerId = orderId)
+  let contactId = ContactId 1
+  let contact1 = 
+    {
+      ContactId=contactId
+      PhoneNumber = PhoneNumber "111-111-1111"
+      EmailAddress = EmailAddress "sd@hkjl.com"
+    }
+  let contact2 = 
+    {
+      ContactId=contactId
+      PhoneNumber = PhoneNumber "111-111-1111"
+      EmailAddress = EmailAddress "sd@hkjl.com"
+    }
+  printfn "compaing 2 contact : %b" (contact1 = contact2)
--- domain/types.fs
+++ domain/types.fs
@@ -1,8 +1,77 @@
 namespace DomainTypes
 
-module BusinessLogic =
-  type BusId = BusId of string
-  type CustomerId = CustomerId of string
-  type Point = { x: float; y: float}
-  type Vehicle = {CurrentLoc: Point; Id: string; Name: Option<string>}
+module rec BusinessLogic =
+  type Result<'Success, 'Failure> =
+    | OK  of 'Success
+    | Error of 'Failure
+  type ProductCode =
+    | Widget of WidgetCode 
+    | Gizmo of GizmoCode
+  type WidgetCode = WidgetCode of string
+  type GizmoCode = GizmoCode of string
+  type OrderQuantity =
+    | Unit of UnitQuantity
+    | Kilogram of KilogramQuantity
+  type UnitQuantity = UnitQuantity of int
+  type KilogramQuantity = KilogramQuantity of decimal
 
+  type CustomerId = CustomerId of int
+  type OrderId = OrderId of int
+  type Undefined = exn
+  type CustomerInfo = Undefined
+  type ShippingAddress = Undefined
+  type BillingAddress = Undefined
+  type OrderLine = Undefined
+  type BillingAmount = Undefined
+  type Order = {
+      CustomerInfo : CustomerInfo
+      ShippingAddress : ShippingAddress
+      BillingAddress : BillingAddress
+      OrderLines : OrderLine list
+      AmountToBill : BillingAmount
+  }
+  type UnvalidatedOrder = Undefined
+  type ValidatedOrder = Undefined
+  type AcknowledgmentSent = Undefined
+  type OrderPlaced = Undefined
+  type BillableOrderPlaced = Undefined
+  type PlaceOrderEvents = {
+    AcknowledgmentSent : AcknowledgmentSent
+    OrderPlaced : OrderPlaced
+    BillableOrderPlaced : BillableOrderPlaced
+  }
+  type PlaceOrder = UnvalidatedOrder -> PlaceOrderEvents
+  type QuoteForm = Undefined
+  type OrderForm = Undefined
+  type CategorizedMail =
+    | Quote of QuoteForm
+    | Order of OrderForm
+
+    type EnvelopeContents = Undefined
+  type CategorizedInboundMail = EnvelopeContents -> CategorizedMail
+  type ProductCatalog = Undefined
+  type PriceOrder = Undefined
+  type CalculatePrices = OrderForm -> ProductCatalog -> PriceOrder
+  type ValidationError = {
+    FieldName : string
+    ErrorDescription : string
+  }
+  type ValidationResponse<'a> = Async<Result<'a, ValidationError list>>
+  type ValidateOrder =
+    UnvalidatedOrder -> ValidationResponse<ValidatedOrder>
+  type ContactId = ContactId of int
+  type PhoneNumber = PhoneNumber of string
+  type EmailAddress = EmailAddress of string
+
+  [<CustomEquality; NoComparison>]
+  type Contact = {
+    ContactId : ContactId
+    PhoneNumber : PhoneNumber
+    EmailAddress : EmailAddress
+  } with
+  override this.Equals(obj) =
+    match obj with
+    | :? Contact as c -> this.ContactId = c.ContactId
+    | _ -> false
+  override this.GetHashCode() =
+    hash this.ContactId
